e0df7294e08b0a64f70c1d4f7931c075f4e299dd
Acerrah002 <henryacerraparera.com>
2025-11-06 11:22:57 -0500

    Initial commit

---
+++ Example.py
@@ -0,0 +1,32 @@
+#This is a int
+x = 1
+
+#This is a Boolean
+y = True
+
+#This is a float
+z = 0.99
+
+#This is a list
+w = ["Apple","Bananas","Oranges"]
+
+#This is a string
+o = "Hello World"
+
+#This is a input
+print(w)
+#userchat = input("These are the fruit options you have, choose an element: ")
+
+#This is a function
+def shop(NUM):
+    #This prints our parameter
+    print(NUM)
+    #in function
+#out of function
+
+#This is calling the function
+#shop(userchat)
+#here i am calling the function
+shop(z)
+#this is the second function
+shop(x)
