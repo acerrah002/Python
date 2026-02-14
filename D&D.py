7ebc413e4593d17f4be8e669f4859c0e00ffb66f
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-02-14 18:00:26 -0500

    Add files via upload

---
+++ D&D.py
@@ -0,0 +1,173 @@
+import random
+
+
+#This is a comment , they are used to communicate with dev without
+#affecting code
+HeroName = "23"
+ClassHero = "Assassin"
+Age = 51
+
+StartLocation = "Tavern of Dragons"
+StarterNPC = "TravernBartender"
+
+#print(StarterNPC,": Welcome traveller",HeroName)
+
+#print(StarterNPC,":","Welcome,",HeroName)
+#inputUser = input("Would you like to go on a quest?: ")
+
+#print(inputUser)
+
+#print("You've been kicked out")
+
+Forestname = "Enchanted Forest"
+
+#print(HeroName, "Has decided to go to go deeper in the forest", Forestname)
+
+#This is a Boolean with is equal to True or False
+#Boolean = input("Would you like to eat the first mushroom we see?: ")
+#Boolean = True
+
+#if Boolean == "True":
+    #print("Yummy")
+#else:
+    #print("You did no eat the mushroom!")
+
+#print("You start feeling dizzy...")
+#print("You wake up in a strange place...")
+
+StrangePlace = "Goblin Terrority"
+GoblinName = "Bob the Goblin"
+
+#print(GoblinName,":", "You've been taken prisoner for stepping foot in our forest >:( ")
+
+#float is any number with a decimal
+CaptureMoney = 24.5
+#print(GoblinName,":", "You must pay, $", CaptureMoney)
+
+UserMoney = 100.0
+
+
+UserMoney = UserMoney - CaptureMoney
+#print("You have paid the goblin $", CaptureMoney)
+#print("You now have $", UserMoney)
+
+#print("THE GOBBLIN TERRORITY IS UNDER ATTACK BY FOREST ELVES!!!")
+UserHealth = 100
+
+#print("You've been hit with an arrow!!!!")
+UserHealth = UserHealth - 10
+
+#print("You see Bob napping forever...")
+
+StealBoolean = True
+
+RandomInt = random.randint(1,4)
+
+if StealBoolean == True:
+    #print("You steal from Bobs pockets")
+    #$533.5
+    #Money got destroyed ge only had $2
+    #$50
+    #Nesting a if statement
+    if RandomInt == 1:
+        #print("You found $50!")
+        UserMoney = UserMoney + 50
+
+    elif RandomInt == 2:
+        #print("You found $2!")
+        UserMoney = UserMoney + 2
+
+    elif RandomInt == 3:
+        #print("You found $533.5!")
+        UserMoney = UserMoney + 533.5
+    else:
+        #print("You found nothing... Matter of fact you dropped some money...")
+        UserMoney = UserMoney - 20
+
+#UserMoney = 609.0
+#Jayden has $50
+#Quicy has $125.5
+UserMoney = (609.0 + 50 + 125.5)/3
+
+#print("You now have $", UserMoney)
+
+#print("You secretly hid inside a elf carrage")
+#print("And coincidently this was the elf generals carrage")
+
+CarrageRandom = random.randint(1,2)
+
+if CarrageRandom == 1:
+    print("The Elf General finds you and notices you were a prisoner to the Goblins")
+    print("He asks you where you'd like to go at a price, since you've already entered the carrage")
+
+    #Room for more
+elif CarrageRandom == 2:
+    print("The Elf General doesn't notice you")
+    #Room for more
+
+#List carry multiple values
+locationOptions = ["Elf City",StartLocation,"SandyShores","FrozenMoutainRange"]
+#this will decided where our campaign continues in the carriage
+choose = random.randint(0,3)
+#outputs our travel location
+#print(locationOptions[choose])
+#This is a variable for the trip cost
+#carriagecost=(choose*50)
+#this updates our money
+#UserMoney=UserMoney-carriagecost
+
+UserMoney = 261.5
+#print(UserMoney)
+#Loop is a condition that repeats
+
+#while(i <= 5)
+#for(i <= HeroName)
+
+#i=0
+
+# <= Less or Equal to
+# == Same values
+# >= More or Equal to
+# -- deincrementing
+# ++ incrementing
+# += incrementing a variable to a another variable
+# -= deincrementing a variable to another variable
+
+#while(i < 5):
+    #print(i)
+    #i += 4
+
+#print("You've arrived in Elf City!")
+#print("it was for free")
+
+LocationTwo = "Elf City"
+#print("You've unlocked the shop")
+
+#Functions in python are blocks that can be used multiple times
+#to make a function in python we start by typing "def"
+
+#Name = ShopUI
+#Parameters everything inside brackets
+
+def ShopUI():
+    #Dictionaries
+    #Dictionaries have Key value pairs
+    #HealthPotion Heals 30 health
+    ShopItems = {"ChestPlate":50,"MEDHealthPot":15,"CrossBow":75,"Dagger":200,"RandomGlowingRock":1,"Bolts10x":20}
+    #print(ShopItems.keys())
+    #print(ShopItems["ChestPlate"])
+    
+    #first we printed the locatioing of the list then the key of the dictionary which gives us the value of the item
+    
+    #Choose to return a value
+
+    #return ShopItems[3]["Dagger"]
+#To activate our function and use it's we need to call it
+#To call a function we do the function name and it's backets but make sure you have the function defined before it
+
+ShopUI()
+#print(ShopUI())
+
+#print(UserMoney)
+#print(UserHealth)
+#Considering Daggers
