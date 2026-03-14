c41531d782a83e1ae5ed2d3e28bcb4e385487238
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-03-14 18:06:41 -0400

    Refactor rock/paper/scissors game structure

--- recap.py
+++ recap.py
@@ -3,42 +3,14 @@
 #we will have out options to pick nested inside a while loop
 #then we will have several if statements to play agasint AI
 import random
-user = input("Type what you'll do: (Rock,Paper,Scissor) ")
-user = user.lower()
-
-print(user)
 choices = ["rock","paper","scissors"]
 AI=random.choice(choices)
-
-if user == "scissor":
-    if AI == "rock":
-        print("AI Won")
-    elif AI == "paper":
-        print("User Won")
-    else:
-        print("Draw")
-
-elif user == "paper":
-    if AI == "rock":
-        print("User Won")
-    elif AI == "paper":
-        print("Draw")
-    else:
-        print("AI Won")
-
-else:
-    if AI == "rock":
-        print("Draw")
-    elif AI == "paper":
-        print("AI Won")
-    else:
-        print("User Won")
 
 Tryagain = input("Want to play?(n/y)")
 while Tryagain != "n":
     user = input("Type what you'll do: (Rock,Paper,Scissor) ")
     user = user.lower()
-    print(user)
+    #print(user)
     choices = ["rock","paper","scissors"]
     AI=random.choice(choices)
 
