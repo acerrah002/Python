d9cd05166019e7cb1a33daffc929bfb3554a1dfe
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-04-11 18:51:33 -0400

    Add keyboard listener to write 'Hello world!'
    Implement keyboard listener to write text and log position.

---
+++ Macro.py
@@ -0,0 +1,11 @@
+import keyboard
+import pyautogui
+
+print("code started")
+while True:
+    if keyboard.read_key()=="right":
+        pyautogui.write("Hello world!")
+        userx , usery = pyautogui.position()
+        print(userx, usery)
+    elif keyboard.read_key()=="up":
+        break
