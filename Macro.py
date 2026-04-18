49702623744900e2ba9aac1f4d586d427b17a8a5
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-04-18 18:05:22 -0400

    Refactor Macro.py for user input and click actions
    Refactor macro functionality to use user input for actions and remove continuous loop.

--- Macro.py
+++ Macro.py
@@ -1,46 +1,25 @@
 import keyboard
 import pyautogui
 
+pyautogui.PAUSE
 print("code started")
-while True:
-    pyautogui.PAUSE = 0.1
-    if keyboard.read_key()=="right":
-        pyautogui.write("Hello world!")
-        userx , usery = pyautogui.position()
-        print(userx, usery)
-        while True: 
-            pyautogui.moveTo(userx, usery)
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            pyautogui.click()
-            if keyboard.read_key()=="up":
-                print("ended code")
-                break
-    elif keyboard.read_key()=="up":
-        print("ended code")
-        break
+user = input("what would you like to do?(1,2)")
+
+def moveandclickfunc(x,y):
+    pyautogui.moveTo(x,y)
+    pyautogui.click()
+
+if user == "1":
+    if keyboard.read_key()=="space":
+        while not (keyboard.is_pressed('right')):
+            for i in range(10):
+                pyautogui.click()
+            #moveandclickfunc(1746,421)
+            #moveandclickfunc(1746,500)
+            #moveandclickfunc(1102,497) 
+elif user == "2":
+    if keyboard.read_key()=="space":
+        print(pyautogui.position())
+print("ended code")
+#Point(x=1102, y=497) cookie
+#Point(x=1746, y=421) grandma
