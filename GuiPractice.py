20dc35d390448397ff878dcc3421be3b1bc7ee0c
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-04-25 11:47:07 -0400

    Implement GUI with mouse position capture
    Added a GUI with buttons to print messages and capture mouse positions.

---
+++ GuiPractice.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import ttk
+import keyboard
+import pyautogui
+
+listofmousepositon=[]
+pyautogui.PAUSE
+
+def printhello():
+    print("Hello World")
+
+def createclickposition():
+    if keyboard.read_key()=="+":
+                while not (keyboard.is_pressed('space')):
+                    if keyboard.on_release('+'):
+                        listofmousepositon.append(pyautogui.position())
+                        print("added position")
+
+root = Tk()
+frm = ttk.Frame(root,padding=10)
+frm.grid()
+ttk.Label(frm,text="Hello World!").grid(column=0,row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=0)
+
+ttk.Label(frm,text="Add Position").grid(column=0,row=1)
+ttk.Button(frm, text="Print",command=printhello).grid(column=1,row=1)
+
+ttk.Label(frm,text="End Code").grid(column=0,row=1)
+ttk.Button(frm, text="Print",command=printhello).grid(column=1,row=1)
+root.mainloop()
