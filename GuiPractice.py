e45d3d37a9448d8e87b06c39760dfa468207db91
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-04-25 11:55:55 -0400

    Update createclickposition function and comment out code
    Modified createclickposition function to recognize 'right' key and print a message. Commented out the end code section for potential future use.

--- GuiPractice.py
+++ GuiPractice.py
@@ -10,11 +10,13 @@ def printhello():
     print("Hello World")
 
 def createclickposition():
-    if keyboard.read_key()=="+":
-                while not (keyboard.is_pressed('space')):
-                    if keyboard.on_release('+'):
+    print("code started")
+    if keyboard.read_key()=="right":
+                print("keyboard recognized")
+                #while not (keyboard.is_pressed('space')):
+                    #if keyboard.on_release('+'):
                         #listofmousepositon.append(pyautogui.position())
-                        print(pyautogui.position())
+                        #print(pyautogui.position())
 
 root = Tk()
 frm = ttk.Frame(root,padding=10)
@@ -25,6 +27,6 @@ ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1,row=0)
 ttk.Label(frm,text="Add Position").grid(column=0,row=1)
 ttk.Button(frm, text="Print",command=createclickposition).grid(column=1,row=1)
 
-ttk.Label(frm,text="End Code").grid(column=0,row=1)
-ttk.Button(frm, text="Print",command=printhello).grid(column=1,row=1)
+#ttk.Label(frm,text="End Code").grid(column=0,row=1)
+#ttk.Button(frm, text="Print",command=printhello).grid(column=1,row=1)
 root.mainloop()
