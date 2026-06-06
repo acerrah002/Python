eebdce4b2a107579d6d57b5aa69545ec3cea5afb
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-06-06 11:29:57 -0400

    Create Nutrition.py

---
+++ AppDev/Nutrition.py
@@ -0,0 +1,50 @@
+import json
+
+def writetoprofile():
+    userDate = input("Enter the date (YYYY-MM-DD): ")
+    userBreakfastCalories = int(input("Enter calories for breakfast: "))
+    userBreakfastProtein = int(input("Enter protein for breakfast: "))
+
+    userLunchCalories = int(input("Enter calories for Lunch: "))
+    userLunchProtein = int(input("Enter protein for Lunch: "))
+
+    userDinnerCalories = int(input("Enter calories for Dinner: "))
+    userDinnerProtein = int(input("Enter protein for Dinner: "))
+
+    totalCalories = userBreakfastCalories + userLunchCalories + userDinnerCalories
+    totalProtein = userBreakfastProtein + userLunchProtein + userDinnerProtein
+
+    user_profile = {
+        #This is in YYYY-MM-DD format
+        "Date" : userDate,
+        "Calories" : totalCalories,
+        #103, 70, 80
+        "Protein" : totalProtein,
+        "Meals":{
+            "Breakfast" : {
+                "Calories" : userBreakfastCalories,
+                "Protein" : userBreakfastProtein
+            },
+
+            "Lunch" : {
+                "Calories" : userLunchCalories,
+                "Protein" : userLunchProtein
+            },
+
+            "Dinner" : {
+                "Calories" : userDinnerCalories,
+                "Protein" : userDinnerProtein}
+          }
+        }
+
+    with open("user_profile.json", "w") as file:
+        json.dump(user_profile, file)
+
+def readfromprofile():
+    #make it to only see the date and calories
+    with open("user_profile.json", "r") as file:
+        user_profile = json.load(file)
+    print(f"Date: {user_profile['Date']}")
+    print(f"Calories: {user_profile['Calories']}")
+
+writetoprofile()
