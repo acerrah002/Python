d15446737c331a4f579a1fbd542fe6b846644164
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-06-13 11:58:53 -0400

    Implement GUI for Nutrition Tracker
    Added a GUI using customtkinter for the Nutrition Tracker.

--- Projects/Nutrition.py
+++ Projects/Nutrition.py
@@ -1,5 +1,18 @@
 import json
 import os
+import customtkinter as ctk 
+
+root = ctk.CTk()
+root.geometry("400x200")
+root.title("Nutrition Tracker")
+root.grid_rowconfigure((0,1),weight=1)
+root.grid_columnconfigure(0,weight=1)
+button1 = ctk.CTkButton(root, text="Calender")
+button2 = ctk.CTkButton(root, text="Track Nutrition")
+button1.grid(row=0, column=0,rowspan=2,sticky="nsew", padx=20, pady=10)
+button2.grid(row=2, column=0,rowspan=2,sticky="nsew", padx=20, pady=10)
+
+root.mainloop()
 
 def askuserinput(meal):
     inputCalories = int(input(f"Enter calories for {meal}: "))
