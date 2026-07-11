b499953deedc351d0f8214ae195ce73157e0668f
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-07-11 12:54:26 -0400

    Update PetFightWithUI.py

--- Python12pm/PetFightWithUI.py
+++ Python12pm/PetFightWithUI.py
@@ -28,6 +28,7 @@ def Attack(HP, label ,MaxDamage, WhichPet):
     print(WhichPet, " was Attcked")
     global Health
     Health = HP
+    button.pack_forget()
 
 def Defend(HP, label, MaxDefense, WhichPet):
     print("Defend")
@@ -36,6 +37,17 @@ def Defend(HP, label, MaxDefense, WhichPet):
     HealthLabel.configure(text="Health: " + str(Health))
     print(WhichPet, " was Defended")
 
+def ShowAttackScreen():
+    #Packs Position where our UI will be
+    HealthLabel.pack(padx=0, pady=20)
+    PetLabel.pack(padx=10, pady=20)
+    label.pack(padx=20, pady=20)
+    button.pack(padx=30, pady=20)
+    button2.pack(padx=30, pady=40)
+
+def ShowMainScreen():
+    pass
+
 app = customtkinter.CTk()
 app.geometry("800x800")
 #Labels
@@ -43,13 +55,14 @@ label = customtkinter.CTkLabel(app, text="NOTHING")
 HealthLabel = customtkinter.CTkLabel(app, text="Health: 100")
 PetLabel = customtkinter.CTkLabel(app,text=pet)
 #Buttons
-button = customtkinter.CTkButton(app, text="Attack", command=lambda: Attack(Health, HealthLabel))
-button2 = customtkinter.CTkButton(app, text="Defend", command=lambda: Defend(Health, HealthLabel))
-#Packs Position where our UI will be
-HealthLabel.pack(padx=0, pady=20)
-PetLabel.pack(padx=10, pady=20)
-label.pack(padx=20, pady=20)
-button.pack(padx=30, pady=20)
-button2.pack(padx=30, pady=40)
+button = customtkinter.CTkButton(app, text="Attack", command=lambda: Attack(Health, HealthLabel, 10, "1"))
+button2 = customtkinter.CTkButton(app, text="Defend", command=lambda: Defend(Health, HealthLabel, 10, "1"))
+#TextBox
+PetText = customtkinter.CTkTextbox(app)
+EnemyText = customtkinter.CTkTextbox(app)
+SubmitPets = customtkinter.CTkButton(app, text="Submit Pets", command=lambda: ShowAttackScreen())
+#Pack TextBox
+PetText.pack(padx=20, pady=20)
+EnemyText.pack(padx=20, pady=40)
 #Runs Application
 app.mainloop()
