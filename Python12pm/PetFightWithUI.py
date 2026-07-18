3eb8f701041be8fcb4679b75e14da54400924772
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2026-07-18 13:04:02 -0400

    Modify ShowAttackScreen to accept parameters

--- Python12pm/PetFightWithUI.py
+++ Python12pm/PetFightWithUI.py
@@ -36,11 +36,14 @@ def Defend(HP, label, MaxDefense, WhichPet):
     HealthLabel.configure(text="Health: " + str(Health))
     print(WhichPet, " was Defended")
 
-def ShowAttackScreen():
+def ShowAttackScreen(pet,enemy):
     #Packs Position where our UI will be
     HealthLabel.pack(padx=0, pady=20)
     PetLabel.pack(padx=10, pady=20)
-    label.pack(padx=20, pady=20)
+    #show our pets on attack screen
+    PetLabel.configure(text=pet)
+    EnemyLabel.configure(text=enemy)
+    EnemyLabel.pack(padx=20, pady=20)
     button.pack(padx=30, pady=20)
     button2.pack(padx=30, pady=40)
 
@@ -50,8 +53,8 @@ def HideMainScreen():
     PetText.pack_forget()
     EnemyText.pack_forget()
     SubmitPets.pack_forget()
-    ShowAttackScreen()
-    pass
+    ShowAttackScreen(PetText.get("0.0", "end"), EnemyText.get("0.0", "end"))
+
 
 def ShowMainScreen():
     pass
@@ -60,7 +63,7 @@ def ShowMainScreen():
 app = customtkinter.CTk()
 app.geometry("800x800")
 #Labels
-label = customtkinter.CTkLabel(app, text="NOTHING")
+EnemyLabel = customtkinter.CTkLabel(app, text="NOTHING")
 HealthLabel = customtkinter.CTkLabel(app, text="Health: 100")
 PetLabel = customtkinter.CTkLabel(app,text=pet)
 #Buttons
@@ -69,7 +72,7 @@ button2 = customtkinter.CTkButton(app, text="Defend", command=lambda: Defend(Hea
 #TextBox
 PetText = customtkinter.CTkTextbox(app)
 EnemyText = customtkinter.CTkTextbox(app)
-SubmitPets = customtkinter.CTkButton(app, text="Submit Pets", command=lambda: HideMainScreen)
+SubmitPets = customtkinter.CTkButton(app, text="Submit Pets", command=lambda: HideMainScreen())
 #Pack TextBox
 PetText.pack(padx=20, pady=20)
 EnemyText.pack(padx=20, pady=40)
